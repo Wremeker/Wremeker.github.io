bd1514948841028bcd103ba2f56b47bb4e379d39
Wremeker <vanayzvonkov@gmail.com>
2018-11-24 16:48:02 +0300

    todo and currency app add

---
+++ currency-app/precache-manifest.c796455b81e04f8e46c4c9e0d5ebd586.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "97d26dac38c3c12c1fca",
+    "url": "/static/js/main.97d26dac.chunk.js"
+  },
+  {
+    "revision": "877a34686c41324e38f7",
+    "url": "/static/js/1.877a3468.chunk.js"
+  },
+  {
+    "revision": "97d26dac38c3c12c1fca",
+    "url": "/static/css/main.a28d5f0a.chunk.css"
+  },
+  {
+    "revision": "4177b5bc0d3150d3b221c78fd9a995e5",
+    "url": "/index.html"
+  }
+];
---
+++ todo-app/precache-manifest.5d4797781e138684defe10f87ce271c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8b9a1b2b2f104149793c",
+    "url": "/static/js/main.8b9a1b2b.chunk.js"
+  },
+  {
+    "revision": "08b04a9df8ac291c87e7",
+    "url": "/static/js/1.08b04a9d.chunk.js"
+  },
+  {
+    "revision": "8b9a1b2b2f104149793c",
+    "url": "/static/css/main.242bb219.chunk.css"
+  },
+  {
+    "revision": "4b953606125cbc0f5e65192eb65e7dae",
+    "url": "/index.html"
+  }
+];
